7bd015e645ac6a1efb1aef8b792195217d41c559
abbas <abbashamidicr@gmail.com>
2025-03-14 11:54:13 +0330

    refactor: simplify header

--- src/components/HeaderForDesktopComp.jsx
+++ src/components/HeaderForDesktopComp.jsx
@@ -29,36 +29,20 @@ function HeaderDesktop() {
           </button>
         </div>
       </div>
-      <div className="w-[80%] mx-auto flex items-center justify-between px-2">
-        <ul className="flex items-center gap-7">
-          <HeaderForDesktopItem />
-          <li className="group cursor-pointer">
-            <p className="hover:text-red-600"> مردانه</p>
-            <div className="absolute bg-slate-400 flex flex-col hidden group-hover:block w-[100%] right-0 top-[15.3vh]">
-              <div>Abbas hamidi</div>
-              <div>Abbas hamidi</div>
-              <div>Abbas hamidi</div>
-              <div>Abbas hamidi</div>
-              <div>Abbas hamidi</div>
-            </div>
-          </li>
-          <li className="cursor-pointer">زنانه</li>
-          <li className="cursor-pointer">بچه گانه</li>
-          <li className="text-gray-200 cursor-pointer"> | </li>
-          <li className="text-red-600 cursor-pointer">
-            <p className="flex items-center gap-1">
-              <i class="fa-solid fa-basket-shopping"></i>
-              فروش ویژه
-            </p>
-          </li>
+
+      <div className="w-4/5 mx-auto flex py-2">
+        <ul className="flex items-center gap-7 grow">
+          <li className="group cursor-pointer">مردانه</li>
         </ul>
-        <div className="relative w-[28%]">
+
+        <div className="bg-gray-100 rounded p-1 flex flex-row w-3/12">
           <input
             type="text"
-            className="bg-gray-100 w-full h-[40px] rounded text-sm p-2 pr-8 focus:outline-none"
+            className="grow text-sm focus:outline-none bg-transparent"
             placeholder="جستجو بین محصولات"
           />
-          <i className="fa-solid fa-search absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"></i>
+
+          <i className="fa-solid fa-search text-gray-500 text-lg px-2"></i>
         </div>
       </div>
     </div>
