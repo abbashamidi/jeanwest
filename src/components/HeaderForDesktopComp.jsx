bf4abd87175e43a8c9eaf56257d3e1cfe04d3b75
abbas <abbashamidicr@gmail.com>
2025-03-14 14:21:57 +0330

    feat: add ImageButton and newest menu content

--- src/components/HeaderForDesktopComp.jsx
+++ src/components/HeaderForDesktopComp.jsx
@@ -33,7 +33,11 @@ function HeaderDesktop() {
       <div className="w-4/5 mx-auto flex items-center py-2">
         <ul className="flex items-center gap-7 grow">
           <HeaderForDesktopItem title={"جدیدترین"} icon={"fa-solid fa-fire"}>
-            <h1>mahdi</h1>
+            <div className="flex w-4/5 justify-evenly items-center mx-auto p-4 gap-4">
+              <ImageButton title={'مردانه'} src={"/images/li-hover-divs/image1.jpg"} />
+              <ImageButton title={'زنانه'} src={"/images/li-hover-divs/image2.jpg"} />
+              <ImageButton title={'بچگانه'} src={"/images/li-hover-divs/image3.png"} />
+            </div>
           </HeaderForDesktopItem>
         </ul>
 
@@ -48,6 +52,22 @@ function HeaderDesktop() {
         </div>
       </div>
     </div>
+  );
+}
+
+function ImageButton({ title, icon, src}) {
+  return (
+    <button className="cursor-pointer relative h-[300px] overflow-hidden aspect-square rounded border border-gray-300">
+      <img
+        className="aspect-square h-full object-cover hover:scale-110 transition-transform"
+        src={src}
+        alt="photo"
+      />
+      <div className="text-white absolute bottom-3 left-4 bg-black/50 px-4 py-1 rounded-full">
+        {title}
+        <i className={`${icon ?? 'fa-solid fa-door-open'} ps-2`}></i>
+      </div>
+    </button>
   );
 }
 
