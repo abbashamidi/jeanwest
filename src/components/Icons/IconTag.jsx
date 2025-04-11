db3fa13aeda2160fbba54b49181b4d25958c1c82
abbas <abbashamidicr@gmail.com>
2025-04-11 19:09:13 +0330

    feat: add IconTag

---
+++ src/components/Icons/IconTag.jsx
@@ -0,0 +1,17 @@
+export default function IconTag() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 16 16"
+      className="w-4 h-4"
+    >
+      <g clipPath="url(#tag_svg__a)">
+        <path
+          stroke="currentColor"
+          d="M4.667 4.667h.006m2.854 9.526-5.9-5.9a1 1 0 0 1-.294-.707V2.333a1 1 0 0 1 1-1h5.253a1 1 0 0 1 .707.293l5.9 5.9a1.617 1.617 0 0 1 0 2.28l-4.386 4.387a1.617 1.617 0 0 1-2.28 0Z"
+        ></path>
+      </g>
+    </svg>
+  );
+}
