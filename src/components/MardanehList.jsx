729175ef0d5bcb41ac168484bd06dfc74cef00b0
abbas <abbashamidicr@gmail.com>
2025-04-18 17:49:36 +0330

    fix: add key attribute to array mapping in MardanehList

--- src/components/MardanehList.jsx
+++ src/components/MardanehList.jsx
@@ -7,8 +7,8 @@ export function MardanehList({ title, icon, buttons, hasImage }) {
       </button>
       {hasImage && <img src={hasImage} className="w-60 h-56 my-2 rounded" />}
       <div className="flex flex-col gap-1.5 pt-2 items-start justify-center">
-        {buttons.map((btn) => (
-          <button className=" text-gray-400 hover:text-black flex items-center gap-1 group">
+        {buttons.map((btn, index) => (
+          <button key={index} className=" text-gray-400 hover:text-black flex items-center gap-1 group">
             {btn}
             <i className="fa-solid fa-arrow-left opacity-0 group-hover:opacity-100"></i>
           </button>
